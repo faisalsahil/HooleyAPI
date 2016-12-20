47f885804635fbb39cfd59b8f9567fcedc4e9c05
Faisal Bhatti <faisalbhatti@appsgenii.eu>
2016-12-20 15:26:44 +0500

    migration error fixed

--- db/migrate/20161219134525_remove_columns_from_events.rb
+++ db/migrate/20161219134525_remove_columns_from_events.rb
@@ -2,7 +2,6 @@ class RemoveColumnsFromEvents < ActiveRecord::Migration[5.0]
   def change
     remove_column :events, :name
     remove_column :events, :country_id
-    remove_column :events, :state_id
     remove_column :events, :city_id
     remove_column :events, :organization
     remove_column :events, :description
