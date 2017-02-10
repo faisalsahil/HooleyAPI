757fdd17ad82535dba4222afe4456ea612da741d
Faisal Bhatti <faisalbhatti@appsgenii.eu>
2017-02-10 16:43:06 +0500

    commented the params in controller

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::CategoriesController < ApplicationController
   def index
-    params = {
-      "auth_token": "111111111",
-    }
+    # params = {
+    #   "auth_token": "111111111",
+    # }
     user_session = UserSession.find_by_auth_token(params[:auth_token])
     if user_session.present?
       resp_data = Category.get_categories(params, user_session.user)
