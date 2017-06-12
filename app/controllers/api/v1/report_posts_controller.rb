dd7e9e119d7467ba7531bd9d97aedc539ff7096b
Faisal Bhatti <faisalbhatti@appsgenii.eu>
2017-06-12 22:55:18 +0500

    commented the params

--- app/controllers/api/v1/report_posts_controller.rb
+++ app/controllers/api/v1/report_posts_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::ReportPostsController < ApplicationController
   
   def create
-    params = {
-      "auth_token": UserSession.last.auth_token,
-      "post_id": 1
-    }
+    # params = {
+    #   "auth_token": UserSession.last.auth_token,
+    #   "post_id": 1
+    # }
     user_session = UserSession.find_by_auth_token(params[:auth_token])
     if user_session.present?
       resp_data = ReportPost.report_a_post(params, user_session.user)
