05e9128a00871c4aef9bed5077b433ccccfc978b
Faisal Bhatti <faisalbhatti@appsgenii.eu>
2017-06-14 15:56:32 +0500

    profile count field error fixed

--- app/models/member_profile.rb
+++ app/models/member_profile.rb
@@ -81,8 +81,8 @@ class MemberProfile < ApplicationRecord
     end
   end
   
-  def profile_fil_attributes
-    hash = self.attributes.slice('photo', 'about', 'handler', 'is_age_visible', 'gender', 'current_city', 'occupation_id', 'home_town', 'employer', 'college_major_id', 'college', 'high_school', 'organization', 'hobbies', 'relationship_status_id', 'political_view_id', 'religion_id', 'language_id', 'ethnic_background_id', 'contact_phone', 'contact_website', :'contact_address', 'linkden_link', 'facebook_link', 'flikker_link', 'twitter_link', 'instagram_link')
+    def profile_fil_attributes
+    hash = self.attributes.slice('photo', 'about', 'handler', 'is_age_visible', 'gender', 'current_city', 'occupation_id', 'home_town', 'employer', 'college_major_id', 'college', 'high_school', 'organization', 'hobbies', 'relationship_status_id', 'political_view_id', 'religion_id', 'language_id', 'ethnic_background_id', 'contact_phone', 'contact_website', 'contact_address', 'linkden_link', 'facebook_link', 'flikker_link', 'twitter_link', 'instagram_link')
     hash.delete_if{|k,v| v.nil? || v.blank?}
     
     u = self.user.attributes.slice('first_name', 'last_name')
